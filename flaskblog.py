c8b6b0c4c841c8de0d6e9e29ece067034912c23c
Mohamed Bouchi <mohamedsubhibouchi@gmail.com>
2019-03-25 21:37:45 +0100

    add route to about page, add run in debug mode

--- flaskblog.py
+++ flaskblog.py
@@ -3,5 +3,15 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+@app.route("/home")
+def home():
     return "Hello World!"
+
+
+@app.route("/about")
+def about():
+    return "About"
+
+
+if __name__ = '__main__':
+    app.run(debug=True)
