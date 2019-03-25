5981b9b028c579b6f13eb64f728adbc8ff908eb1
Mohamed Bouchi <mohamedsubhibouchi@gmail.com>
2019-03-25 22:28:42 +0100

    add layout file, home and about template, bootstarp and main css file

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
+
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'author1',
+        'title': 'post1',
+        'content': 'content1',
+        'date_posted': 'april 20, 2019'
+    },
+    {
+        'author': 'author2',
+        'title': 'post2',
+        'content': 'content2',
+        'date_posted': 'april 22, 2019'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "Hello World!"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "About"
+    return render_template('about.html', title='about')
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
