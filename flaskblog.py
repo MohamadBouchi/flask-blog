852fdb0fcbcb234a5bc23a5e44b9c83b1bc303a9
Mohamed Bouchi <mohamedsubhibouchi@gmail.com>
2019-03-26 20:32:50 +0100

    add register and login template, authentication and flash messages, install flask-wtf

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '31da0ee3646ae04f456db0a344bab17d'
 
 posts = [
     {
@@ -36,7 +38,25 @@ def about():
     return render_template('about.html', title='about')
 
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+        flash('you have been logged in', 'success')
+        return redirect(url_for('home'))
+    else:
+        flash('login unsuccessful', 'danger')
+    return render_template('login.html', title='login', form=form)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-video 3
