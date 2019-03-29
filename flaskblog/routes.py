fd0483695e61b505c32509dfaf64e24c7ee0e167
Mohamed Bouchi <mohamedsubhibouchi@gmail.com>
2019-03-29 23:29:26 +0100

    edit account template

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 from flaskblog.models import User, Post
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -78,5 +78,6 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    img_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    form = UpdateAccountForm()
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
